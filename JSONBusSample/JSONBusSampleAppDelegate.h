c12d02bedc7981fb6d8ffd781bc28c90114a4224
Chris Brody <chris.brody@gmail.com>
2013-01-02 20:56:11 +0100

    Add webview, www/index.html, JSON communications, & fix iOS version

--- JSONBusSample/JSONBusSampleAppDelegate.h
+++ JSONBusSample/JSONBusSampleAppDelegate.h
@@ -7,8 +7,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JSONBusSampleAppDelegate : UIResponder <UIApplicationDelegate>
+@interface JSONBusSampleAppDelegate : UIResponder <UIApplicationDelegate, UIWebViewDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIWebView *myview;
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
 
 @end
--- JSONBusSample/JSONBusSampleAppDelegate.m
+++ JSONBusSample/JSONBusSampleAppDelegate.m
@@ -12,15 +12,47 @@
 - (void)dealloc
 {
     [_window release];
+    [_myview release];
     [super dealloc];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.myview = [[[UIWebView alloc] initWithFrame:[self.window bounds]] autorelease];
+    [self.window addSubview:self.myview];
+    self.myview.delegate = self;
+
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"www"];
+    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self.myview loadHTMLString:html baseURL:nil];
+
     [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if ([[[request URL]absoluteString] hasPrefix:@"jsonrequest:"]) {
+        NSString *s1 = [[[request URL] absoluteString] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        NSLog(@"JSON: %@", s1);
+
+        NSString *js = [s1 substringFromIndex:12];
+        NSLog(@"decoding js: %@", js);
+        NSData *d = [js dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *j = [NSJSONSerialization JSONObjectWithData:d options:kNilOptions error:nil];
+        NSLog(@"jsonrequest: %@", j);
+        NSLog(@"jsonrequest[\"a\"]: %@", [j objectForKey:@"a"]);
+
+        [self.myview stringByEvaluatingJavaScriptFromString:@"window.testcb('{b:2}')"];
+
+        return NO;
+    }
+    
     return YES;
 }
 
