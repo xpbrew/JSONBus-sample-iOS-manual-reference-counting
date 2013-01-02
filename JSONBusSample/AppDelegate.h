99c4a8c0426d2e6e633d2973ff397aa18f2057d6
Chris Brody <chris.brody@gmail.com>
2013-01-02 20:32:22 +0100

    Initial version

---
+++ JSONBusSample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JSONBusSample
+//
+//  Created by Brodyspark on 1/2/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSONBusSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JSONBusSample
+//
+//  Created by Brodyspark on 1/2/13.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
